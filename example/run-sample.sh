37764278bd912ba17576501e462f862bd45cf700
David Venable <dlv@amazon.com>
2022-01-27 17:49:06 -0600

    Created a sample Data Prepper plugin along with a script to run it within Data Prepper.

---
+++ example/run-sample.sh
@@ -0,0 +1,5 @@
+#!/bin/sh
+
+DATA_PREPPER_JAR=${DATA_PREPPER_JAR:-../../data-prepper/data-prepper-core/build/libs/data-prepper-core-1.3.0-SNAPSHOT.jar}
+
+java -cp ../build/libs/data-prepper-sample-plugin-1.0-SNAPSHOT.jar:${DATA_PREPPER_JAR} com.amazon.dataprepper.DataPrepperExecute config/say-hello-pipeline.yaml config/data-prepper-config.yaml
---
+++ src/main/java/com/amazon/dataprepper/plugins/HelloPlugin.java
@@ -0,0 +1,39 @@
+package com.amazon.dataprepper.plugins;
+
+import com.amazon.dataprepper.model.annotations.DataPrepperPlugin;
+import com.amazon.dataprepper.model.event.Event;
+import com.amazon.dataprepper.model.processor.Processor;
+import com.amazon.dataprepper.model.record.Record;
+
+import java.util.Collection;
+
+/**
+ * This sample plugin adds a field named 'hello' with a value of 'world'
+ * to each Event.
+ */
+@DataPrepperPlugin(name = "hello", pluginType = Processor.class)
+public class HelloPlugin implements Processor<Record<Event>, Record<Event>> {
+
+    @Override
+    public Collection<Record<Event>> execute(final Collection<Record<Event>> collection) {
+        collection
+                .forEach(eventRecord -> eventRecord.getData().put("hello", "world"));
+
+        return collection;
+    }
+
+    @Override
+    public void prepareForShutdown() {
+
+    }
+
+    @Override
+    public boolean isReadyForShutdown() {
+        return true;
+    }
+
+    @Override
+    public void shutdown() {
+
+    }
+}
